9ba105de4d28ea41880b6c357f0e687c6930b66e
Mikkel Birch <mib@kvalitetsit.dk>
2023-06-29 10:24:13 +0200

    use tenantid

--- KitNugs/Services/UserServiceKeycloak.cs
+++ KitNugs/Services/UserServiceKeycloak.cs
@@ -10,13 +10,15 @@ namespace KitNugs.Services
     public class UserServiceKeycloak : IUserService
     {
         private readonly string _realm;
-        private readonly ILogger<UserServiceStub> _logger;
+        private readonly string _tokenAttributeName;
+        private readonly ILogger<UserServiceKeycloak> _logger;
         private IUsersApi _usersApi;
         private readonly IMapper<UserResponse, UserRepresentation> _userMapper;
 
-        public UserServiceKeycloak(IServiceConfiguration configuration, ILogger<UserServiceStub> logger, IUsersApi usersApi, IMapper<UserResponse,UserRepresentation> userMapper)
+        public UserServiceKeycloak(IServiceConfiguration configuration, ILogger<UserServiceKeycloak> logger, IUsersApi usersApi, IMapper<UserResponse, UserRepresentation> userMapper)
         {
             _realm = configuration.GetConfigurationValue(ConfigurationVariables.RealmToManage);
+            _tokenAttributeName = configuration.GetConfigurationValue(ConfigurationVariables.TenantAttributeName);
             _logger = logger;
             _usersApi = usersApi;
             _userMapper = userMapper;
@@ -24,12 +26,18 @@ namespace KitNugs.Services
 
         public async Task CreateUser(string tenantId, UserResponse userToCreate)
         {
-            await _usersApi.PostUsersAsync(_realm, _userMapper.MapTo(userToCreate));
+            UserRepresentation userRepresentation = _userMapper.MapTo(userToCreate);
+            var attributes = new Dictionary<string, List<string>>(){
+                {_tokenAttributeName, new List<string>() { tenantId }}
+            };
+            userRepresentation.Attributes = attributes;
+
+            await _usersApi.PostUsersAsync(_realm, userRepresentation);
         }
 
         public async Task<IList<UserResponse>> GetUsersForTenant(string tenantId)
         {
-            var keycloakUsers = await _usersApi.GetUsersAsync(_realm);
+            var keycloakUsers = await _usersApi.GetUsersAsync(_realm, q: _tokenAttributeName + ":" + tenantId);
 
             var toReturn = keycloakUsers.Select(_userMapper.MapFrom);
 
